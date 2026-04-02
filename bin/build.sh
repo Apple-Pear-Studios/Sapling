d3ce4132bbceec7e0d3e8e111deebcf9e226beac
Sebastian2852 <sebastian2852@proton.me>
2026-04-02 11:36:22 +0200

    Update release workflow

---
+++ bin/build.sh
@@ -0,0 +1,6 @@
+mkdir build
+
+rojo build packages/sapling -o build/sapling.rbxm
+rojo build packages/lifelikeCharactersService -o build/lifelikeCharactersService.rbxm
+rojo build packages/queryBuilder -o build/queryBuilder.rbxm
+rojo build packages/registry -o build/registry.rbxm
